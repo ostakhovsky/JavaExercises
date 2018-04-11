88e618ccd62584ae99df5f35712b9a1973515887
Olga Didenko <odidenko@linkedin.com>
2018-04-10 21:12:54 -0700

    Chapter 3. Some examples and exercises

--- src/chapter3/examples/NameDialog_3_18.java
+++ src/chapter3/examples/NameDialog_3_18.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 Fig. 3.18
  */
 
-public class NameDialog {
+public class NameDialog_3_18 {
   public static void main(String[] args) {
     String name = JOptionPane.showInputDialog("What is your name?");
     String message = String.format("Welcome, %s, to Java Programming!", name);
